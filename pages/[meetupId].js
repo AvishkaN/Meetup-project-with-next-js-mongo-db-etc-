42ab88db0884137021dd3f76595d48b53bfd7588
NIRMITHA <avishkanirmitha@gmail.com>
2021-08-20 20:43:53 +0530

    next ok level

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -4,34 +4,29 @@ import MeetupDetails from './../components/meetups/MeetupDetails'
 function meetUpId(props) {
     return (
         <>
-        {/* {console.log(props)} */}
-        <MeetupDetails 
-        image={props.meetupData.image} 
-        title={props.meetupData.title} 
-        address={props.meetupData.address} 
-        description={props.meetupData.description}
-        />
+            <MeetupDetails 
+            image={props.meetupData.image} 
+            title={props.meetupData.title} 
+            address={props.meetupData.address} 
+            description={props.meetupData.description}
+            />
         </>
     )
 }
 
 export async function getStaticPaths(){
 
- MongoClient.connect(); 
-  const client=await MongoClient
+//  MongoClient.connect(); 
+  
+ const client=await MongoClient
     .connect('mongodb+srv://jayathissaMongoUser:asdasedfrasdefsadfsadfsdfdsf@cluster0.2u4vz.mongodb.net/meetups?retryWrites=true&w=majority')
   const db=client.db();
   const meetupCollection=db.collection('meetups');
 
   const meetups=await meetupCollection.find({},{_id:1}).toArray();
 
-  console.log(typeof meetups[0]._id.toString());
+  client.close();
 
-     const paths=meetups.map(meetup=>({
-            params:{meetupId:meetup._id.toString()},
-        }))
-
-        console.log(paths);
 
     return {
         fallback:false,
@@ -39,24 +34,19 @@ export async function getStaticPaths(){
             params:{meetupId:meetup._id.toString()},
         }))
         
-    //    paths:[
-    //         {
-    //             params:{
-    //                 meetupId:'m1',
-    //             }
-    //         },
-    //         {
-    //             params:{
-    //                 meetupId:'m2',
-    //             }
-    //         }
-    //     ],
     };
 };
 
 
 export async function getStaticProps(context){
-    // console.log(context.params.meetupId);
+
+    console.log(context.params.meetupId);
+
+    // api
+
+
+
+    //
 
     return {
         props:{
